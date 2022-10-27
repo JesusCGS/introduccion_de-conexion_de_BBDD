a63fc5328af47675f1b95cda7b31d72ba735e357
JesusCGS <abcenu2002@gmail.com>
2022-10-27 13:56:27 +0200

    parte usuario
    terminado

--- introJDBC/src/main/java/dominio/Usauario_dao.java
+++ introJDBC/src/main/java/dominio/Usauario_dao.java
@@ -17,6 +17,9 @@ import java.util.List;
  */
 public class Usauario_dao {
     private static final String SQL_SELECT ="SELECT * FORM usuario ";
+    private static final String SQL_INSERT ="INSERT INTO usuario(contraseña,nombre)VALUES (?,?)";
+    private static final String SQL_UPDATE ="UPDATE usuario SET contraseña=?,nombre=? WHERE idusario=?";
+    private static final String SQL_DELETE ="DELETE FROM usuario WHERE idusuario=? ";
     
     
     
@@ -46,6 +49,96 @@ public class Usauario_dao {
         return usuarios;
     
     }
+    public int insertar(Usuario usuario){
+
+            //declaro e incializo mis variables
+             Connection conn = null;
+             PreparedStatement stm = null;
+             int registros=0;
+              try {  
+            //1. establecer la conexion
+                conn = getConnecction();
+            //2. reparo mi instrucion para ejecutarla contra la base de datos 
+                stm= conn.prepareStatement(SQL_INSERT);
+            //asignar los valores de los ? de la consulta    
+                stm.setString(1,usuario.getContraseña());
+                stm.setString(2,usuario.getNombre());
+                
+                
+
+            //3.ejcuto el array
+                registros= stm.executeUpdate();
+
+              }catch(SQLException ex){
+                  ex.printStackTrace(System.out);
+
+              }finally{
+                 try {
+                     close(stm);
+                     close(conn);
+                 } catch (SQLException ex) {
+                      ex.printStackTrace(System.out);
+                 }
+              }
+              return registros;
+    }
+    
+    public int actualizar(Usuario usuario){
+
+            //declaro e incializo mis variables
+             Connection conn = null;
+             PreparedStatement stm = null;
+             int registros=0;
+              try {  
+            //1. establecer la conexion
+                conn = getConnecction();
+            //2. reparo mi instrucion para ejecutarla contra la base de datos 
+                stm= conn.prepareStatement(SQL_UPDATE);
+            //asignar los valores de los ? de la consulta    
+                stm.setString(1,usuario.getContraseña());
+                stm.setString(2,usuario.getNombre());
+                stm.setInt(3,usuario.getIdusario());
+
+            //3.ejcuto el array
+                registros= stm.executeUpdate();
+
+              }catch(SQLException ex){
+                  ex.printStackTrace(System.out);
+
+              }finally{
+                 try {
+                     close(stm);
+                     close(conn);
+                 } catch (SQLException ex) {
+                      ex.printStackTrace(System.out);
+                 }
+              }
+              return registros;
+    }
+    public int delete (Usuario usuario){
+        Connection conn = null;
+        PreparedStatement stm = null;
+                 int registros=0;
+                  try{  
+                       conn = getConnecction();
+                       stm= conn.prepareStatement(SQL_DELETE);
+                       stm.setInt(1, usuario.getIdusario());
+                       registros = stm.executeUpdate();
+                       
+                  }catch(SQLException ex){
+                  ex.printStackTrace(System.out);
+                  }finally{
+                 try {
+                     close(stm);
+                     close(conn);
+                 } catch (SQLException ex) {
+                      ex.printStackTrace(System.out);
+                 }
+              }
+              return registros;
+    }
+    
+
     
     
 }
